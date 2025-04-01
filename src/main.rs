cfa70c903f050c95e6164f58f092f7a6dc8c34f8
z3ntl3 <z3ntl3discord@gmail.com>
2025-04-01 23:01:31 +0200

    04/01/2025 23:01:31

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ use tokio::sync::{mpsc, Mutex};
 // Ensuring everything works pretty solid and fast.
 //
 // Aside from those facts using Broadcast would've required us to implement clone on some sort of body as it propogates the same data by cloning the value
-// to multiple consumers. But we didn't want that. That would mean when the handler runs in parallel with several clients, they all would see the same random image.
+// to multiple consumers, also inheriting more memory consumption.
+//
+// But we didn't want that. Because when the handler runs in parallel with several clients, they all would see the same random image.
 //
 // For those reasons the current way of handling the situation seems like one of the proper fits for the task
 #[debug_handler]
