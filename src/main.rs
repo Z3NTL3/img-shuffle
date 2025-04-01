53eac839e8244c8dda47b3bb7c3790b4b69200ff
z3ntl3 <z3ntl3discord@gmail.com>
2025-04-01 22:37:50 +0200

    04/01/2025 22:37:50

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::{sync::Arc, time};
-
 use axum::{body::Body, debug_handler, extract::State, response::Response, Router};
 use errors::AppError;
 use rand::prelude::*;
 use tokio_util::io::ReaderStream;
 use tokio::sync::{mpsc, Mutex};
 
+// Arc<Mutex<Receiver<T>>> is needed because a mpsc channel can have one receiver. By using Arc we share ownership.
+// An Async Mutex is needed due to the primitives of Arc enforcing us otherwise taking &mut self would've failed on line 17.
+//
+// Async Mutexes are not blocking by literal meaning, as recv().await yields back control to the async runtime until the task can be resumed
+// Ensuring everything works pretty solid and fast.
+//
+// Aside from those facts using Broadcast would've required us to implement clone on some sort of body as it propogates the same data by cloning the value
+// to multiple consumers. But we didn't want that. That would mean when the handler runs in parallel with several clients, they all would see the same random image.
+//
+// For those reasons the current way of handling the situation seems like one of the proper fits for the task
 #[debug_handler]
 async fn random_image(State(receiver): State<Arc<Mutex<mpsc::Receiver<Body>>>>) -> Result<Response<Body>, errors::AppError> {    
     let bench = time::Instant::now();
